520d50054206627fa49d61820f5a31ac429630c0
thepape <panach88@gmail.com>
2017-01-04 11:06:05 +0100

    CREATION MAVEN PROJECT

---
+++ src/main/java/org/m2acsi/ProjetRestApplication.java
@@ -0,0 +1,12 @@
+package org.m2acsi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetRestApplication.class, args);
+	}
+}
