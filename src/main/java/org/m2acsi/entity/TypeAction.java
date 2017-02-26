0284597c6e01f30bc48d8176d6484709992670f0
thepape <panach88@gmail.com>
2017-02-26 15:58:50 +0100

    feat(TypeAction) : ajout d'autres types
    et ajout d'un attribut permettant de matcher avec le nom de privilege
    associe en BDD coté utilisateur

--- src/main/java/org/m2acsi/entity/TypeAction.java
+++ src/main/java/org/m2acsi/entity/TypeAction.java
@@ -1,20 +1,34 @@
 package org.m2acsi.entity;
 
 public enum TypeAction {
-	ETUDE("Etude"),
-	APPROBATION("Approbation"),
-        REFUS("Refus"),
-        CREATION_COMMANDE("Creation d'une commande"),
-	CLOTURE("Cloture de la demande");
+	ETUDE("Etude","etude"),
+	ETUDE_DETAILLEE("Etude détaillée","etude_detaillee"),
+	APPROBATION("Approbation","approbation"),
+        REFUS("Refus","refus"),
+        CREATION_ORDRE_MAINTENANCE("Création d'un ordre de maintenance","creation_ordre_maintenance"),
+        CREATION_COMMANDE("Creation d'une commande","creation_commande_reparation"),
+        CONFIRMATION_REPARATION("Confirmation de la réparation","confirmation_reparation"),
+	CLOTURE("Cloture de la demande","");
         
 	
 	private String nom = "";
 	
-	TypeAction(String nom){
+	/**
+	 * suffixe correspondant au nom du privilege qu'il faut posseder pour poster cette action.
+	 * ex : perm_post_action_etude_detaillee => etude_detaillee
+	 */
+	private String nomPrivilege = "";
+	
+	TypeAction(String nom, String nomPrivilege){
 		this.nom = nom;
+		this.nomPrivilege = nomPrivilege;
 	}
 	
 	public String toString(){
 		return this.nom;
+	}
+	
+	public String getNomPrivilege(){
+		return this.nomPrivilege;
 	}
 }
